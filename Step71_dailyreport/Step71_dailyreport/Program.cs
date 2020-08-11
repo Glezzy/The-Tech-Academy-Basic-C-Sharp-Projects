aa92725029c67ca8a22f1e43f7e409a286eb9ec8
Glezzy <gsg@pdx.edu>
2020-08-11 14:05:40 -0600

    update

---
+++ Step71_dailyreport/Step71_dailyreport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Step71_dailyreport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            
+            Console.WriteLine("What is your name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer \"True or False\".");
+
+            Console.WriteLine("Where there any positive experiences you would like to share? Please give specific.");
+
+            Console.WriteLine("Is there any other feedback your would like to share? Please be specific.");
+
+            Console.WriteLine("How many hours did you study today?");
+
+
+
+        }
+    }
+}
--- step54_acme/step54_acme/Program.cs
+++ step54_acme/step54_acme/Program.cs
@@ -7,11 +7,7 @@ namespace studentquestionsAssignment
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Welcome to Acme Accounting Systems.\nRemember, we're \"accounting\" on you!");
-=======
-            Console.WriteLine("Welcome to Acme Accounting Systems.\n Remember, we're \"accounting\" on you!");
->>>>>>> 2f57e5c19c702152a982423715e3d1d71169599e
             Console.Read();
         }
     }
