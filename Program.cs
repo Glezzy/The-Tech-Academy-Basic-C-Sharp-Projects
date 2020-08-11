877a731186c663bd060b4365b2b1e7e7d8451e7a
Greg Glesener <37390181+Glezzy@users.noreply.github.com>
2020-08-11 09:42:53 -0600

    Acme Accounting Systems Console print out

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems.\nRemember, we're \"accounting\" on you!");
+            string name = Console.ReadLine();
+        }
+    }
+}
