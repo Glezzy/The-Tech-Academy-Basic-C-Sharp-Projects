b966e36efe992e0f7ee02e2de6c6b71e5f4940f3
Glezzy <gsg@pdx.edu>
2020-08-12 11:29:08 -0600

    step 100 completed!

---
+++ step100_carinsurance/step100_carinsurance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace step100_carinsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speeding = Convert.ToInt32(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Qualified?: ");
+            Console.WriteLine(age > 15 && dui == false && speeding <= 3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
