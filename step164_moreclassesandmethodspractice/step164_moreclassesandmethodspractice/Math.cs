a92db0ee88f36352bbc4f7c015768ce4fc38ceda
Glezzy <gsg@pdx.edu>
2020-08-24 12:06:33 -0600

    step 164 assignment and continuation of blackjack

---
+++ step164_moreclassesandmethodspractice/step164_moreclassesandmethodspractice/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace step164_moreclassesandmethodspractice
+{
+    public class Math
+    {
+        public int Operation(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Operation(decimal x, decimal y)
+        {
+            return Convert.ToInt32(x - y);
+        }
+
+        public int Operation(string x, string y)
+        {
+            return Convert.ToInt32(x) * Convert.ToInt32(y);
+        }
+    }
+}
