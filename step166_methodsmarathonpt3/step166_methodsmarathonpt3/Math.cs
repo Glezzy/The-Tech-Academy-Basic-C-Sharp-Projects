a528153291e52b3d983faf0251bdd3ac9acd5158
Glezzy <gsg@pdx.edu>
2020-08-24 14:55:40 -0600

    step166 assignment completed

---
+++ step166_methodsmarathonpt3/step166_methodsmarathonpt3/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace step166_methodsmarathonpt3
+{
+    public class Math
+    {
+        public void Operation(int x, int y)
+        {
+            int result = x + 10;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ step166_methodsmarathonpt3/step166_methodsmarathonpt3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace step166_methodsmarathonpt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Operation(x: 10, y: 25);
+        }
+    }
+}
