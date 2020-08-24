f0ddf8e16dbcbcee8ea0d32c0fa826b470c6e782
Glezzy <gsg@pdx.edu>
2020-08-24 15:40:19 -0600

    step 169 method overloading

---
+++ step169_methodswithoverloading/step169_methodswithoverloading/Divide.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace step169_methodswithoverloading
+{
+    class Divide
+    {
+        public int Operation(int x)
+        {
+            return x / 2;
+        }
+        public void Operation(int x, out int result)
+        {
+            result = x / 2;
+        }
+    }
+
+    public static class MyStaticClass
+    {
+        public static string desc = "Hello, I am a static class.";
+
+        public static string Desc { get => desc; set => desc = value; }
+    }
+}
+
---
+++ step169_methodswithoverloading/step169_methodswithoverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace step169_methodswithoverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide divide = new Divide();
+
+            Console.WriteLine("Please enter a number to be divided by 2: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            divide.Operation(input, out int result);
+
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine(input + " / 2 = " + result);
+
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine(MyStaticClass.Desc);
+            Console.ReadLine();
+
+        }
+    }
+}
