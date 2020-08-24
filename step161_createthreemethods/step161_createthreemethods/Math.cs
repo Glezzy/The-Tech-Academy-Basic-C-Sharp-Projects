8025f1224f8a81a7aa4c071b1a905bef79d36c10
Glezzy <gsg@pdx.edu>
2020-08-24 11:32:18 -0600

    start of 21 and step 161 assignment

---
+++ step161_createthreemethods/step161_createthreemethods/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace step161_createthreemethods
+{
+    public class Math
+    {
+        public int Add(int x)
+        {
+            return x + 34;
+        }
+        public int multiply(int x)
+        {
+            return x * 2;
+        }
+        public int divide(int x)
+        {
+            return x / 2;
+        }
+    }
+}
---
+++ step161_createthreemethods/step161_createthreemethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step161_createthreemethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a number to be added by 34, multiplied by 3, and divided by 9: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number + " + 34 = " + math.Add(number) + "\n" + number + " * 2 = " + math.multiply(number) + "\n" + number + " / 2 = " + math.divide(number));
+
+            Console.ReadLine();
+        }
+    }
+}
