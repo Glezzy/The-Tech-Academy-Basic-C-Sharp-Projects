8445722f723e769eefa3fd41a12526fef1d5ce2b
Glezzy <gsg@pdx.edu>
2020-08-24 13:15:24 -0600

    step 165 assignment completed

---
+++ step165_classesandmethodspart2/step165_classesandmethodspart2/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace step165_classesandmethodspart2
+{
+   public class Math
+    {
+        public int Operation(int x, int y = 1)
+        {
+            return (x + y / 4);
+        }
+    }
+}
---
+++ step165_classesandmethodspart2/step165_classesandmethodspart2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace step165_classesandmethodspart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            try
+            {
+                Console.WriteLine("Please enter a single number or two numbers whose sum will be divided by 4:  ");
+                Console.Write(Environment.NewLine);
+                Console.WriteLine("Please enter your first number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter your second number: ");
+                string temp = Console.ReadLine();
+
+                if (temp == "")
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Calculating...");
+                    Console.WriteLine(number1 + "/ 4 = " + math.Operation(number1));
+                }
+                else
+                {
+                    int number2 = Convert.ToInt32(temp);
+
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Calculating...");
+                    Console.WriteLine("(" + number1 + " + " + number2 + ") / 4 = " + math.Operation(number1, number2));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(Environment.NewLine);
+                Console.WriteLine("Error! " + ex.Message);
+            }
+                
+
+                Console.ReadLine();
+        }
+    }
+}
