b4e4e75cddb0fec0888b133ca87a8c28479d2863
Glezzy <gsg@pdx.edu>
2020-08-27 18:35:57 -0600

    Polymorphism assignment completed

---
+++ step237_polymorphism/step237_polymorphism/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace step237_polymorphism
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, my name is: ");
+            base.SayName();
+        }
+    }
+}
---
+++ step237_polymorphism/step237_polymorphism/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace step237_polymorphism
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
