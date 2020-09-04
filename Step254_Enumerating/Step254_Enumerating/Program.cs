fc93d239d0579e4ee1513213e2e1f94a117c659d
Glezzy <gsg@pdx.edu>
2020-09-03 18:44:13 -0600

    Step 254 complete

---
+++ Step254_Enumerating/Step254_Enumerating/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step254_Enumerating
+{
+    class Program
+    {
+        static void Main()
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), DateTime.Today.AddDays(1).DayOfWeek.ToString().ToLower());
+            bool check = true;
+
+            while (check)
+            {
+                try
+                {
+                    if (day.ToString() != today.ToString().ToLower())
+                    {
+                        Console.WriteLine("Please enter the current day of the week: ");
+                        day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine().ToLower());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Correct, {0} is the current day of the week.", day.ToString());
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Please enter an actual day of the week.");
+                    Console.Write(Environment.NewLine);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
