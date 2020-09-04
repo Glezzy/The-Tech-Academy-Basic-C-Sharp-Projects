cc2feb938cfcc2236f21f79abea356eace705fe8
Glezzy <gsg@pdx.edu>
2020-09-04 08:35:20 -0600

    Step 260 decimals assignment

---
+++ Step260/Step260/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Step260
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 2.5799m;
+
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
