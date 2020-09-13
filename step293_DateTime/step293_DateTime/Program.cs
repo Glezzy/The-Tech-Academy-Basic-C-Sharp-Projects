93cfaeeb99f9187af3869e02481b65b0edbad1ad
Glezzy <gsg@pdx.edu>
2020-09-13 10:32:23 -0600

    DateTime assignment

---
+++ step293_DateTime/step293_DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace step293_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The current time is: " + currentTime);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number: ");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("This will add 7 hours to the time you entered. : " + currentTime.AddHours(userNumber));
+        }
+    }
+}
